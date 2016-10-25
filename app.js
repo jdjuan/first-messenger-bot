e6654ada301aad949a920b8618464382f3fb3baf
Juan David Herrera Marin <Juan David Herrera Marin>
2016-10-24 20:03:16 -0500

    Added missing semicolon

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var bodyParser = require('body-parser')
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
