c84f917b4790c13b46a807a4c592fd3ebfbedb49
Juan David Herrera Marin <Juan David Herrera Marin>
2016-10-24 19:27:18 -0500

    added webhook get route

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 1234) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port: ' + app.get('port'));
 });
