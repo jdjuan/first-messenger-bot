faf9aeeb0ad54ea1307160cacec09b18be625766
Juan David Herrera Marin <Juan David Herrera Marin>
2016-10-24 20:07:19 -0500

    Added use of json with body parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json())
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
