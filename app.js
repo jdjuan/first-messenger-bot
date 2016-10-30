bf217d12b8753a9bcae2020fd7d312f464faf020
Juan David Herrera Marin <Juan David Herrera Marin>
2016-10-29 21:34:32 -0500

    Ommited security check

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.use(bodyParser.json())
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.json({ verify: verifyRequestSignature }));
+// app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 // App Secret can be retrieved from the App Dashboard
 const APP_SECRET = config.get('appSecret');
@@ -65,27 +65,27 @@ app.post('/webhook', function(req, res) {
  * https://developers.facebook.com/docs/graph-api/webhooks#setup
  *
  */
-function verifyRequestSignature(req, res, buf) {
-  var signature = req.headers["x-hub-signature"];
+// function verifyRequestSignature(req, res, buf) {
+//   var signature = req.headers["x-hub-signature"];
 
-  if (!signature) {
-    // For testing, let's log an error. In production, you should throw an
-    // error.
-    console.error("Couldn't validate the signature.");
-  } else {
-    var elements = signature.split('=');
-    var method = elements[0];
-    var signatureHash = elements[1];
+//   if (!signature) {
+//     // For testing, let's log an error. In production, you should throw an
+//     // error.
+//     console.error("Couldn't validate the signature.");
+//   } else {
+//     var elements = signature.split('=');
+//     var method = elements[0];
+//     var signatureHash = elements[1];
 
-    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
-                        .update(buf)
-                        .digest('hex');
+//     var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+//                         .update(buf)
+//                         .digest('hex');
 
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
-    }
-  }
-}
+//     if (signatureHash != expectedHash) {
+//       throw new Error("Couldn't validate the request signature.");
+//     }
+//   }
+// }
 
 app.listen(app.get('port'), function() {
     console.log('Example app listening on port: ' + app.get('port'));
