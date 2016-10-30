588615933988122a19748c20211d8034d4d40a12
Juan David Herrera Marin <Juan David Herrera Marin>
2016-10-29 22:08:44 -0500

    Added button message manager

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var app = express();
 app.use(bodyParser.json())
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
-// app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 // App Secret can be retrieved from the App Dashboard
 const APP_SECRET = config.get('appSecret');
@@ -57,36 +56,6 @@ app.post('/webhook', function(req, res) {
         res.sendStatus(200);
     }
 });
-
-/*
- * Verify that the callback came from Facebook. Using the App Secret from
- * the App Dashboard, we can verify the signature that is sent with each
- * callback in the x-hub-signature field, located in the header.
- *
- * https://developers.facebook.com/docs/graph-api/webhooks#setup
- *
- */
-// function verifyRequestSignature(req, res, buf) {
-//   var signature = req.headers["x-hub-signature"];
-
-//   if (!signature) {
-//     // For testing, let's log an error. In production, you should throw an
-//     // error.
-//     console.error("Couldn't validate the signature.");
-//   } else {
-//     var elements = signature.split('=');
-//     var method = elements[0];
-//     var signatureHash = elements[1];
-
-//     var expectedHash = crypto.createHmac('sha1', APP_SECRET)
-//                         .update(buf)
-//                         .digest('hex');
-
-//     if (signatureHash != expectedHash) {
-//       throw new Error("Couldn't validate the request signature.");
-//     }
-//   }
-// }
 
 app.listen(app.get('port'), function() {
     console.log('Example app listening on port: ' + app.get('port'));
--- scripts/responseManager.js
+++ scripts/responseManager.js
@@ -66,6 +66,37 @@ module.exports = {
 
         facebookAPI.callSendAPI(messageData);
     },
+    sendButtonMessage: function(recipientId) {
+        var messageData = {
+            recipient: {
+                id: recipientId
+            },
+            message: {
+                attachment: {
+                    type: "template",
+                    payload: {
+                        template_type: "button",
+                        text: "This is test text",
+                        buttons: [{
+                            type: "web_url",
+                            url: "https://www.oculus.com/en-us/rift/",
+                            title: "Open Web URL"
+                        }, {
+                            type: "postback",
+                            title: "Trigger Postback",
+                            payload: "DEVELOPED_DEFINED_PAYLOAD"
+                        }, {
+                            type: "phone_number",
+                            title: "Call Phone Number",
+                            payload: "+16505551234"
+                        }]
+                    }
+                }
+            }
+        };
+
+        facebookAPI.callSendAPI(messageData);
+    },
     sendTextMessage: function(recipientId, messageText) {
         var messageData = {
             recipient: {
