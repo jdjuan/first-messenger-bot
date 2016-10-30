2ff6b77c0176df93c3e21d7732d3f0fef0c2c623
Juan David Herrera Marin <Juan David Herrera Marin>
2016-10-29 23:38:54 -0500

    Added chrono photos

--- scripts/eventManager.js
+++ scripts/eventManager.js
@@ -55,26 +55,31 @@ module.exports = {
                     responseManager.sendTypingOff(senderID);
                     break;
                 default:
-                    responseManager.sendTextMessage(senderID, messageText);
+                    responseManager.sendMainMenu(senderID);
             }
         } else if (messageAttachments) {
-            responseManager.sendTextMessage(senderID, "Message with attachment received");
+            responseManager.sendMainMenu(senderID);
         }
     },
     receivedPostback: function(event) {
         var senderID = event.sender.id;
         var recipientID = event.recipient.id;
         var timeOfPostback = event.timestamp;
-
-        // The 'payload' param is a developer-defined field which is set in a postback 
-        // button for Structured Messages. 
-        var payload = event.postback.payload;
-
         console.log("Received postback for user %d and page %d with payload '%s' " +
             "at %d", senderID, recipientID, payload, timeOfPostback);
+        // The 'payload' param is a developer-defined field which is set in a postback 
+        // button for Structured Messages. 
 
-        // When a postback is called, we'll send a message back to the sender to 
-        // let them know it was successful
-        responseManager.sendTextMessage(senderID, "Postback called");
+        var payload = event.postback.payload;
+        switch (payload) {
+            case 'ver-fotos':
+                responseManager.sendTextMessage(senderID, "Ver fotos");
+                responseManager.sendPhotos(senderID);
+                break;
+            default:
+                responseManager.sendTextMessage(senderID, "Payload not received");
+                break;
+        }
+
     }
 }
--- scripts/responseManager.js
+++ scripts/responseManager.js
@@ -4,6 +4,109 @@ var config = require('config');
 const SERVER_URL = config.get('serverURL');
 
 module.exports = {
+    sendMainMenu: function(recipientId) {
+         var messageData = {
+            recipient: {
+                id: recipientId
+            },
+            message: {
+                attachment: {
+                    type: "template",
+                    payload: {
+                        template_type: "generic",
+                        elements: [{
+                            title: "¡Hola!",
+                            subtitle: "Soy el bot de Juan Herrera. Escoge una de las siguientes opciones:",
+                            buttons: [{
+                                type: "postback",
+                                title: "Ver fotos",
+                                payload: "ver-fotos",
+                            }, {
+                                type: "web_url",
+                                url: "http://www.juandavidherrera.com/",
+                                title: "Ver página web"
+                            }],
+                        }]
+                    }
+                }
+            }
+        };
+        facebookAPI.callSendAPI(messageData);
+    },
+    sendPhotos: function(recipientId) {
+         var messageData = {
+            recipient: {
+                id: recipientId
+            },
+            message: {
+                attachment: {
+                    type: "template",
+                    payload: {
+                        template_type: "generic",
+                        elements: [{
+                            title: "2 Años",
+                            image_url: SERVER_URL + "/pics/002.jpg"
+                        }, {
+                            title: "3 Años",
+                            image_url: SERVER_URL + "/pics/003.jpg"
+                        }, {
+                            title: "5 Años",
+                            image_url: SERVER_URL + "/pics/005.jpg"
+                        }, {
+                            title: "6 Años",
+                            image_url: SERVER_URL + "/pics/006.jpg"
+                        }, {
+                            title: "7 Años",
+                            image_url: SERVER_URL + "/pics/007.jpg"
+                        }, {
+                            title: "8 Años",
+                            image_url: SERVER_URL + "/pics/008.jpg"
+                        }, {
+                            title: "9 Años",
+                            image_url: SERVER_URL + "/pics/009.jpg"
+                        }, {
+                            title: "13 Años",
+                            image_url: SERVER_URL + "/pics/013.jpg"
+                        }, {
+                            title: "14 Años",
+                            image_url: SERVER_URL + "/pics/014.jpg"
+                        }, {
+                            title: "15 Años",
+                            image_url: SERVER_URL + "/pics/015.jpg"
+                        }, {
+                            title: "16 Años",
+                            image_url: SERVER_URL + "/pics/016.jpg"
+                        }, {
+                            title: "17 Años",
+                            image_url: SERVER_URL + "/pics/017.jpg"
+                        }, {
+                            title: "18 Años",
+                            image_url: SERVER_URL + "/pics/018.jpg"
+                        }, {
+                            title: "19 Años",
+                            image_url: SERVER_URL + "/pics/019.jpg"
+                        }, {
+                            title: "20 Años",
+                            image_url: SERVER_URL + "/pics/020.jpg"
+                        }, {
+                            title: "21 Años",
+                            image_url: SERVER_URL + "/pics/021.jpg"
+                        }, {
+                            title: "22 Años",
+                            image_url: SERVER_URL + "/pics/022.jpg"
+                        }, {
+                            title: "22 Años y medio",
+                            image_url: SERVER_URL + "/pics/022-.jpg"
+                        }, {
+                            title: "23 Años",
+                            image_url: SERVER_URL + "/pics/023.jpg"
+                        }]
+                    }
+                }
+            }
+        };
+        facebookAPI.callSendAPI(messageData);
+    },
     sendGenericMessage: function(recipientId) {
         var messageData = {
             recipient: {
