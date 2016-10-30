df9df54aa53350a7095e66ae3b6f78f17fea65d4
Juan David Herrera Marin <Juan David Herrera Marin>
2016-10-29 20:40:59 -0500

    Moved dependency between managers

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var request = require('request');
 var eventManager = require('./scripts/eventManager');
 var app = express();
 
--- scripts/facebookAPI.js
+++ scripts/facebookAPI.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 module.exports = {
     callSendAPI: function(messageData) {
         request({
